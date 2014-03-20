e107efb9ee05b6ac726a84582cbd38754d2b297d
thecountchuckula <rainsdaddy@gmail.com>
2014-03-19 21:52:11 -0700

    Mob drops update
    Bats now drop gold nuggets.
    Ghasts drop gold ingots instead of tears.

--- Common/tcc/tccTech/BlockLeatherBlock.java
+++ Common/tcc/tccTech/BlockLeatherBlock.java
@@ -12,29 +12,31 @@ public class BlockLeatherBlock extends Block {
 
 	public BlockLeatherBlock(int par1, Material par2Material) {
 		super(par1, par2Material);
-        this.setCreativeTab(CreativeTabs.tabDecorations);
-        this.setHardness(0.4F);
-        this.setUnlocalizedName("leatherBlock");
+		this.setCreativeTab(CreativeTabs.tabDecorations);
+		this.setHardness(0.4F);
+		this.setUnlocalizedName("leatherBlock");
 	}
+
 	/**
-     * Returns the ID of the items to drop on destruction.
-     */
-    @Override
-    public int idDropped(int par1, Random par2Random, int par3) {
-        return Item.leather.itemID;
-    }
+	 * Returns the ID of the items to drop on destruction.
+	 */
+	@Override
+	public int idDropped(int par1, Random par2Random, int par3) {
+		return Item.leather.itemID;
+	}
 
-    @Override
-    public void registerIcons(IconRegister reg) {
-        blockIcon = reg.registerIcon(tcctechInfo.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring(5));
-    }
+	@Override
+	public void registerIcons(IconRegister reg) {
+		blockIcon = reg.registerIcon(tcctechInfo.MOD_ID.toLowerCase() + ":"
+				+ this.getUnlocalizedName().substring(5));
+	}
 
-    /**
-     * Returns the quantity of items to drop on block destruction.
-     */
-    @Override
-    public int quantityDropped(Random par1Random) {
-        return 9;
-    }
+	/**
+	 * Returns the quantity of items to drop on block destruction.
+	 */
+	@Override
+	public int quantityDropped(Random par1Random) {
+		return 9;
+	}
 
 }
--- Common/tcc/tccTech/tcctechInfo.java
+++ Common/tcc/tccTech/tcctechInfo.java
@@ -20,51 +20,66 @@ public class tcctechInfo {
 	public static Item denseIronIngot;
 	public static Item bedrockWallPlacer;
 	public static Item rock;
-		
-	public static final String NAME = "Mow The Lawn UHC Add-On";
+
+	public static final String NAME = "Mow The Lawn UHC Mod";
 	public static final String MOD_ID = "mtluhc";
 	public static final String version = "v1.6-1.6.4";
 	public static final ItemStack beef = new ItemStack(Item.beefRaw, 2, 0);
-	public static final ItemStack bonemeal = new ItemStack(Item.dyePowder, 1, 15);
-	public static final ItemStack fireball = new ItemStack(Item.fireballCharge, 1, 0);
+	public static final ItemStack bonemeal = new ItemStack(Item.dyePowder, 1,
+			15);
+	public static final ItemStack fireball = new ItemStack(Item.fireballCharge,
+			1, 0);
 	public static final ItemStack torches = new ItemStack(Block.torchWood, 8, 0);
 	public static final ItemStack coal = new ItemStack(Item.coal, 1, 0);
 	public static final ItemStack charCoal = new ItemStack(Item.coal, 1, 1);
 	public static final ItemStack diamond = new ItemStack(Item.diamond, 1, 1);
 	public static final ItemStack iron = new ItemStack(Item.ingotIron, 1, 1);
 	public static final ItemStack leather = new ItemStack(Item.leather, 1, 1);
-	public static final ItemStack helmetLeather = new ItemStack(Item.helmetLeather, 1, 1);
-	public static final ItemStack plateLeather = new ItemStack(Item.plateLeather, 1, 1);
-	public static final ItemStack legsLeather = new ItemStack(Item.legsLeather, 1, 1);
-	public static final ItemStack bootsLeather = new ItemStack(Item.bootsLeather, 1, 1);
-	public static final ItemStack helmetIron = new ItemStack(Item.helmetIron, 1, 1);
-	public static final ItemStack plateIron = new ItemStack(Item.plateIron, 1, 1);
+	public static final ItemStack helmetLeather = new ItemStack(
+			Item.helmetLeather, 1, 1);
+	public static final ItemStack plateLeather = new ItemStack(
+			Item.plateLeather, 1, 1);
+	public static final ItemStack legsLeather = new ItemStack(Item.legsLeather,
+			1, 1);
+	public static final ItemStack bootsLeather = new ItemStack(
+			Item.bootsLeather, 1, 1);
+	public static final ItemStack helmetIron = new ItemStack(Item.helmetIron,
+			1, 1);
+	public static final ItemStack plateIron = new ItemStack(Item.plateIron, 1,
+			1);
 	public static final ItemStack legsIron = new ItemStack(Item.legsIron, 1, 1);
-	public static final ItemStack bootsIron = new ItemStack(Item.bootsIron, 1, 1);
-	public static final ItemStack helmetDiamond = new ItemStack(Item.helmetDiamond, 1, 1);
-	public static final ItemStack plateDiamond = new ItemStack(Item.plateDiamond, 1, 1);
-	public static final ItemStack legsDiamond = new ItemStack(Item.legsDiamond, 1, 1);
-	public static final ItemStack bootsDiamond = new ItemStack(Item.bootsDiamond, 1, 1);
+	public static final ItemStack bootsIron = new ItemStack(Item.bootsIron, 1,
+			1);
+	public static final ItemStack helmetDiamond = new ItemStack(
+			Item.helmetDiamond, 1, 1);
+	public static final ItemStack plateDiamond = new ItemStack(
+			Item.plateDiamond, 1, 1);
+	public static final ItemStack legsDiamond = new ItemStack(Item.legsDiamond,
+			1, 1);
+	public static final ItemStack bootsDiamond = new ItemStack(
+			Item.bootsDiamond, 1, 1);
 	public static final ItemStack stick = new ItemStack(Item.stick, 1, 0);
-	public static final ItemStack cobble = new ItemStack(Block.cobblestone, 1, 0);
-	
+	public static final ItemStack cobble = new ItemStack(Block.cobblestone, 1,
+			0);
 
+	public static void itemInit() {
+		denseIronIngot = new ItemDenseIronIngot(tcctechMain.denseIronIngotID);
+		diamondIngot = new ItemDiamondIngot(tcctechMain.diamondIngotID);
+		bedrockWallPlacer = new ItemBedrockWallPlacer(
+				tcctechMain.bedrockWallPlacerID);
+		rock = new ItemRock(tcctechMain.rockID);
 
-	public static void itemInit(){
-			denseIronIngot = new ItemDenseIronIngot(tcctechMain.denseIronIngotID);
-			diamondIngot = new ItemDiamondIngot(tcctechMain.diamondIngotID);
-			bedrockWallPlacer = new ItemBedrockWallPlacer(tcctechMain.bedrockWallPlacerID);
-			rock = new ItemRock(tcctechMain.rockID);
-			
 	}
+
 	public static void blockInit() {
-		leatherBlock = new BlockLeatherBlock(tcctechMain.leatherBlockID, Material.cloth);
+		leatherBlock = new BlockLeatherBlock(tcctechMain.leatherBlockID,
+				Material.cloth);
 		MinecraftForge.setBlockHarvestLevel(leatherBlock, 0, "Axe", 0);
 		GameRegistry.registerBlock(leatherBlock, "Block of Leather");
 	}
-	
+
 	public static void craftingR() {
-		if((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
+		if ((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
 			tcctechInfo.RemoveRecipe(new ItemStack(Item.helmetLeather, 1, 0));
 			tcctechInfo.RemoveRecipe(new ItemStack(Item.plateLeather, 1, 0));
 			tcctechInfo.RemoveRecipe(new ItemStack(Item.legsLeather, 1, 0));
@@ -79,111 +94,65 @@ public class tcctechInfo {
 			tcctechInfo.RemoveRecipe(new ItemStack(Item.bootsDiamond, 1, 0));
 		}
 	}
-	
+
 	public static void crafting() {
-		if((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
-			//Hard Armor Recipes
-			//Leather
+		if ((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
+			// Hard Armor Recipes
+			// Leather
 			GameRegistry.addRecipe(new ItemStack(Item.helmetLeather, 1, 0),
-	        		"lll",
-	        		"l l",
-	        		'l', leatherBlock);
+					"lll", "l l", 'l', leatherBlock);
 			GameRegistry.addRecipe(new ItemStack(Item.plateLeather, 1, 0),
-	        		"l l",
-	        		"lll",
-	        		"lll",
-	        		'l', leatherBlock);
+					"l l", "lll", "lll", 'l', leatherBlock);
 			GameRegistry.addRecipe(new ItemStack(Item.legsLeather, 1, 0),
-	        		"lll",
-	        		"l l",
-	        		"l l",
-	        		'l', leatherBlock);
+					"lll", "l l", "l l", 'l', leatherBlock);
 			GameRegistry.addRecipe(new ItemStack(Item.bootsLeather, 1, 0),
-	        		"l l",
-	        		"l l",
-	        		'l', leatherBlock);
-			//Iron
-			GameRegistry.addRecipe(new ItemStack(Item.helmetIron, 1, 0),
-	        		"lll",
-	        		"l l",
-	        		'l', denseIronIngot);
-			GameRegistry.addRecipe(new ItemStack(Item.plateIron, 1, 0),
-	        		"i i",
-	        		"iii",
-	        		"iii",
-	        		'i', denseIronIngot);
-			GameRegistry.addRecipe(new ItemStack(Item.legsIron, 1, 0),
-	        		"iii",
-	        		"i i",
-	        		"i i",
-	        		'i', denseIronIngot);
-			GameRegistry.addRecipe(new ItemStack(Item.bootsIron, 1, 0),
-	        		"i i",
-	        		"i i",
-	        		'i', denseIronIngot);
-			//Diamond
+					"l l", "l l", 'l', leatherBlock);
+			// Iron
+			GameRegistry.addRecipe(new ItemStack(Item.helmetIron, 1, 0), "lll",
+					"l l", 'l', denseIronIngot);
+			GameRegistry.addRecipe(new ItemStack(Item.plateIron, 1, 0), "i i",
+					"iii", "iii", 'i', denseIronIngot);
+			GameRegistry.addRecipe(new ItemStack(Item.legsIron, 1, 0), "iii",
+					"i i", "i i", 'i', denseIronIngot);
+			GameRegistry.addRecipe(new ItemStack(Item.bootsIron, 1, 0), "i i",
+					"i i", 'i', denseIronIngot);
+			// Diamond
 			GameRegistry.addRecipe(new ItemStack(Item.helmetDiamond, 1, 0),
-	        		"ddd",
-	        		"d d",
-	        		'd', diamondIngot);
+					"ddd", "d d", 'd', diamondIngot);
 			GameRegistry.addRecipe(new ItemStack(Item.plateDiamond, 1, 0),
-	        		"d d",
-	        		"ddd",
-	        		"ddd",
-	        		'd', diamondIngot);
+					"d d", "ddd", "ddd", 'd', diamondIngot);
 			GameRegistry.addRecipe(new ItemStack(Item.legsDiamond, 1, 0),
-	        		"ddd",
-	        		"d d",
-	        		"d d",
-	        		'd', diamondIngot);
+					"ddd", "d d", "d d", 'd', diamondIngot);
 			GameRegistry.addRecipe(new ItemStack(Item.bootsDiamond, 1, 0),
-	        		"d d",
-	        		"d d",
-	        		'd', diamondIngot);
-			GameRegistry.addRecipe(new ItemStack(denseIronIngot, 1, 0),
-					"ii",
-					"ii",
-					'i', Item.ingotIron);
-	        GameRegistry.addRecipe(new ItemStack(diamondIngot, 1, 0),
-					"dd",
+					"d d", "d d", 'd', diamondIngot);
+			GameRegistry.addRecipe(new ItemStack(denseIronIngot, 1, 0), "ii",
+					"ii", 'i', Item.ingotIron);
+			GameRegistry.addRecipe(new ItemStack(diamondIngot, 1, 0), "dd",
 					'd', Item.diamond);
 		}
 		GameRegistry.addSmelting(Item.eyeOfEnder.itemID, fireball, 0.8F);
 		GameRegistry.addShapelessRecipe(new ItemStack(rock, 9, 0), cobble);
-		GameRegistry.addRecipe(cobble,
-				"rrr",
-				"rrr",
-				"rrr",
-				'r', rock);
-		GameRegistry.addRecipe(torches,
-				"c",
-				"p",
-				'c', coal,
-				'p', Block.planks);
-		GameRegistry.addRecipe(torches,
-				"c",
-				"p",
-				'c', charCoal,
-				'p', Block.planks);
-		GameRegistry.addRecipe(new ItemStack(leatherBlock, 1, 0),
-        		"lll",
-        		"lll",
-        		"lll",
-        		'l', Item.leather);
+		GameRegistry.addRecipe(cobble, "rrr", "rrr", "rrr", 'r', rock);
+		GameRegistry.addRecipe(torches, "c", "p", 'c', coal, 'p', Block.planks);
+		GameRegistry.addRecipe(torches, "c", "p", 'c', charCoal, 'p',
+				Block.planks);
+		GameRegistry.addRecipe(new ItemStack(leatherBlock, 1, 0), "lll", "lll",
+				"lll", 'l', Item.leather);
 
-		if((tcctechMain.mowlawn == true) && (tcctechMain.mowlawn != false)) {
+		if ((tcctechMain.mowlawn == true) && (tcctechMain.mowlawn != false)) {
 			MinecraftForge.addGrassSeed(stick, 20);
 			MinecraftForge.addGrassSeed(new ItemStack(rock, 1, 0), 25);
 			MinecraftForge.addGrassSeed(new ItemStack(Item.potato), 5);
 			MinecraftForge.addGrassSeed(new ItemStack(Item.rottenFlesh), 5);
 			MinecraftForge.addGrassSeed(new ItemStack(Item.carrot), 5);
 			MinecraftForge.addGrassSeed(new ItemStack(Item.appleRed), 2);
-			MinecraftForge.addGrassSeed(new ItemStack(Item.dyePowder, 1, 15), 1);
+			MinecraftForge
+					.addGrassSeed(new ItemStack(Item.dyePowder, 1, 15), 1);
 		}
 	}
-	
+
 	public static void names() {
-		if((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
+		if ((tcctechMain.hardarmor == true) && (tcctechMain.hardarmor != false)) {
 			LanguageRegistry.addName(denseIronIngot, "Dense Iron Ingot");
 			LanguageRegistry.addName(diamondIngot, "Diamond Ingot");
 			LanguageRegistry.addName(leatherBlock, "Block of Leather");
@@ -191,24 +160,26 @@ public class tcctechInfo {
 			LanguageRegistry.addName(rock, "Rock");
 		}
 	}
-	
-    protected static void RemoveRecipe(ItemStack resultItem) {
-        ItemStack recipeResult = null;
-        ArrayList recipes = (ArrayList) CraftingManager.getInstance().getRecipeList();
-        for (int scan = 0; scan < recipes.size(); scan++) {
-            IRecipe tmpRecipe = (IRecipe) recipes.get(scan);
-            if (tmpRecipe instanceof ShapedRecipes) {
-                ShapedRecipes recipe = (ShapedRecipes) tmpRecipe;
-                recipeResult = recipe.getRecipeOutput();
-            }
-            if (tmpRecipe instanceof ShapelessRecipes) {
-                ShapelessRecipes recipe = (ShapelessRecipes) tmpRecipe;
-                recipeResult = recipe.getRecipeOutput();
-            }
-            if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
-                System.out.println("[tccTech] Removed Recipe: " + recipes.get(scan) + " -> " + recipeResult);
-                recipes.remove(scan);
-            }
-        }
-    }
+
+	protected static void RemoveRecipe(ItemStack resultItem) {
+		ItemStack recipeResult = null;
+		ArrayList recipes = (ArrayList) CraftingManager.getInstance()
+				.getRecipeList();
+		for (int scan = 0; scan < recipes.size(); scan++) {
+			IRecipe tmpRecipe = (IRecipe) recipes.get(scan);
+			if (tmpRecipe instanceof ShapedRecipes) {
+				ShapedRecipes recipe = (ShapedRecipes) tmpRecipe;
+				recipeResult = recipe.getRecipeOutput();
+			}
+			if (tmpRecipe instanceof ShapelessRecipes) {
+				ShapelessRecipes recipe = (ShapelessRecipes) tmpRecipe;
+				recipeResult = recipe.getRecipeOutput();
+			}
+			if (ItemStack.areItemStacksEqual(resultItem, recipeResult)) {
+				System.out.println("[tccTech] Removed Recipe: "
+						+ recipes.get(scan) + " -> " + recipeResult);
+				recipes.remove(scan);
+			}
+		}
+	}
 }
